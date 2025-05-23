408444c779322e2da26785c33b654eaf9641e8c2
Lucas2882-byte <lucaswebsite@free.fr>
2025-05-23 09:24:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -219,6 +219,7 @@ for statut in ["à faire", "en cours", "terminé"]:
                 # Récupère le nom du client
                 nom_client = row[18] if row[18] else "—"
                 couleur_client = couleur_depuis_nom(nom_client)
+                st.markdown(f"<div style='background-color:{couleur_client}; color:white; padding:10px;'>TEST BADGE</div>", unsafe_allow_html=True)
                 
                 st.markdown(f"""
                 <div style='padding: 15px; border: 1px solid #444; border-radius: 12px; margin-bottom: 15px; background-color: #111;'>
