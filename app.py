43fe694b89c7b5eebb34a822758f59ce90c20d04
Lucas2882-byte <lucaswebsite@free.fr>
2025-05-23 09:45:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -326,6 +326,14 @@ for statut in ["à faire", "en cours", "terminé"]:
                     conn.commit()
                     upload_db_to_github()
                     st.success(f"✅ État mis à jour avec succès – statut : {nouveau_statut}")
+                    
+                if st.button("🗑️ Supprimer cette fiche", key=f"delete_btn_{fiche_id}"):
+                    cursor.execute("DELETE FROM fiches WHERE id = ?", (fiche_id,))
+                    conn.commit()
+                    upload_db_to_github()
+                    st.success(f"🗑️ Fiche {fiche_id} supprimée avec succès.")
+                    st.experimental_rerun()
+
 
                     
 
